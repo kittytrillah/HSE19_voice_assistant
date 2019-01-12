1e1181a64f13b9397c12db377ba37fdf5d6c42ac
Victor <v6_dev@inbox.ru>
2019-01-13 00:19:37 +0300

    Completed feature 1

--- src/features/record_note.feature
+++ src/features/record_note.feature
@@ -6,6 +6,23 @@ Feature: Record a note
     Given service is active
     When user pronounce "test record"
     And program recognize input
+    And program starts recording speech
+    And program recorded text Any piece of text
+    And piece of recorded part is finished
     #And time interval since last record < 3 sec # This is coroutine, so start this from step below
-    Then record state is activated
-    And user pronounces what has to be recorded
+    Then note is recorded
+    And user pronounces A tag
+    And tag saved with a note
+
+
+    Scenario: Record with an existing tag
+    Given service is active
+    When user pronounce "test record"
+    And program recognize input
+    And program starts recording speech
+    And program recorded text Any piece of text
+    And piece of recorded part is finished
+    #And time interval since last record < 3 sec # This is coroutine, so start this from step below
+    Then note is recorded
+    And user pronounces A tag
+    And tag saved with a note
--- src/features/steps/record_note.py
+++ src/features/steps/record_note.py
@@ -1,12 +1,18 @@
 from behave import given, when, then
 import speech_recognition as sr
 import asyncio
+import re
+import sqlite3
 from os import path
 get_file = lambda f: path.join(path.dirname(path.realpath(__file__)), "/materials/" + f.lower() + ".wav")
 test_file = lambda f: path.join(path.dirname(path.realpath(__file__)), "materials/" + "speech_test.wav")
 test_record = lambda f: path.join(path.dirname(path.realpath(__file__)), "materials/" + "record.wav")
+tag_books = lambda f: path.join(path.dirname(path.realpath(__file__)), "materials/" + "books.wav")
 ttime = False
 
+
+currentst = '' #this is a global string constantly updating variable containing recorded speech
+dirpath = path.dirname(path.realpath(__file__))
 
 ########## BEHAVE PART ###########
 
@@ -41,12 +47,50 @@ def recognize_command(context):
         pass
 
 
+@when("program recorded text {text}") #in future speech to text should be done in coroutine
+def record_text(context, text):
+    audio_file = test_file(text)
+    context.audio_file = audio_file
+    with sr.AudioFile(context.audio_file) as source:
+        audio = context.recognizer.record(source)
+    context.recognized_text = context.recognizer.recognize_google(audio)
+    currentst = context.recognized_text # sort of mock for the future updates
+    print('recorded current: ' + currentst)
+    print('recognized : ' + context.recognized_text)
+    return 'Text recorded'
 
-#@when("time interval since last record < 3 sec")
-async def state_control(context):
+
+@when("program starts recording {text}")
+def user_reads_text(context, text): #tag_name
+    #audio_file = get_file(text) #Use this for non-test purposes
+    audio_file = test_file(text)
+    context.audio_file = audio_file
+    pass
+
+
+@when("piece of recorded part is finished")
+def find_text(context):
+    # here the coroutine with regex will be started in future, currently "hack" version below
+    with open(dirpath + "/materials/" + "orwell_1984.txt", 'r') as myfile:
+        data = myfile.read().replace('\n', '')
+        find = context.recognized_text
+        words = re.findall("\w+", data)
+        result = [x for x in find if x in words]
+        print('data: ' + data)
+        print('currenst: ' + context.recognized_text)
+        print(result) # in future, the pronounced part of text should be highlighted
+    return 'Text found'
+
+
+async def state_control():
     ttime = True
     await asyncio.sleep(3)
     ttime = False
+    return ttime
+
+
+async def checktextexistence(): #Mock for the future speech-to-text update
+    await asyncio.sleep(0.1)
     return ttime
 
 
@@ -61,18 +105,45 @@ async def state_control(context):
 #     context.time_interval_satisfied = True
 
 
-@then("record state is activated")
-def tag_recorded(context):
-    #assert context.reply == "Tag Recorded" // leave it for representation
-    #context.tag_added = [tag_name]
+@then("note is recorded")
+def tag_record(context):
+    conn = sqlite3.connect(dirpath + "/notes_db.db")
+    c = conn.cursor()
+    c.execute(
+        "Create TABLE if not exists notes (tag TEXT,content TEXT)")
+    try:
+        pass
+        #c.execute() Execute our values not to lose note data
+        #conn.commit()
+    except Exception as err:
+        print(err)
+        pass
+    context.reply = "Note Recorded. Tag?"
+    startcoroutine()
     pass
 
 
-@then("user pronounces what has to be recorded")
-def succeed_code_in_status_bar(context):
-    #assert context.reply == "Tag recorded: " + tag_name
-    pass
+@then("user pronounces {text}")
+def when_user_pronounced_tag(context, text): #tag_name
+    audio_file = tag_books(text)
+    if ttime:
+        context.audio_file = audio_file # Somewhat timer for operation recognition
 
+
+@then("tag saved with a note")
+def tag_save(context): #tag_name
+    conn = sqlite3.connect(dirpath + "/notes_db.db")
+    c = conn.cursor()
+    c.execute(
+        "Create TABLE if not exists notes (tag TEXT,content TEXT)")
+    try:
+        pass
+        # c.execute() Execute our values not to lose note data
+        # conn.commit()
+    except Exception as err:
+        print(err)
+        pass
+    context.reply = "Tag recorded"
 
 ########## ADDITIONAL STUFF #############
 
@@ -80,7 +151,15 @@ def succeed_code_in_status_bar(context):
 def startcoroutine():
     loop = asyncio.get_event_loop()
     future = asyncio.Future()
-    task = asyncio.ensure_future(state_control(""))
+    task = asyncio.ensure_future(state_control())
     loop.run_until_complete(task)
     print('Coroutine done')
     return 'Completed'
+
+
+def refreshsearchcoroutine():
+    loop = asyncio.get_event_loop()
+    future = asyncio.Future()
+    task = asyncio.ensure_future(state_control())
+    loop.run_until_complete(task)
+    return 'Search part done'
