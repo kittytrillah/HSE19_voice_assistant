f719672b3d1e4831b21b2239d880b5ddd1c7c88f
Victor <v6_dev@inbox.ru>
2019-01-13 01:26:40 +0300

    Added feature 3

---
+++ src/features/info.feature
@@ -0,0 +1,20 @@
+# Created by Victor at 13.01.2019
+Feature: Info output
+
+  Scenario: Output info onto a screen
+    Given service is enabled
+    When user chooses a quote
+    And user pronounce: "source"
+    And program recognizes operation
+    Then program reads meta
+    And program shows meta info
+
+
+  Scenario: Output info onto a screen
+    Given service is enabled
+    When user chooses a quote
+    And user pronounce: "find author"
+    And program recognizes operation
+    Then program reads meta
+    And looks for author info using Wikipedia API
+    And program shows info
---
+++ src/features/steps/info.py
@@ -0,0 +1,70 @@
+from behave import given, when, then
+import speech_recognition as sr
+import asyncio
+import re
+import sqlite3
+from os import path
+get_file = lambda f: path.join(path.dirname(path.realpath(__file__)), "/materials/" + f.lower() + ".wav")
+test_file = lambda f: path.join(path.dirname(path.realpath(__file__)), "materials/" + "speech_test.wav")
+test_record = lambda f: path.join(path.dirname(path.realpath(__file__)), "materials/" + "record.wav")
+tag_books = lambda f: path.join(path.dirname(path.realpath(__file__)), "materials/" + "books.wav")
+quotes = lambda f: path.join(path.dirname(path.realpath(__file__)), "materials/" + "show_me_the_quotes.wav")
+source = lambda f: path.join(path.dirname(path.realpath(__file__)), "materials/" + "source.wav")
+ttime = False
+
+
+
+currentst = '' #this is a global string constantly updating variable containing recorded speech
+dirpath = path.dirname(path.realpath(__file__))
+
+
+########## BEHAVE PART ###########
+
+
+@given("service is enabled")
+def record_competed(context):
+    context.recognizer = sr.Recognizer()
+
+
+@when("user chooses a quote")
+def when_user_pronounced_tag(context): #tag_name
+    #mock
+    return 'Quote has been chosen'
+
+
+@when("user pronounce: {text}")
+def when_user_pronounced_source(context, text): #tag_name
+    #audio_file = get_file(text) #Use this for non-test purposes
+    audio_file = source(text)
+    context.audio_file = audio_file
+
+
+@when("program recognizes operation")
+def recognize_command(context):
+    with sr.AudioFile(context.audio_file) as source:
+        audio = context.recognizer.record(source)
+    context.recognized_command = context.recognizer.recognize_sphinx(audio)
+    print('recognized? : ' + context.recognized_command)
+    if context.recognized_command == "source":
+        print('recognized show')
+    elif context.recognized_command == "find author":
+        print('recognized save')
+
+@then("program reads meta")
+def read_result(context):
+    print("Reading meta") #mock
+
+
+@then("program shows meta info")
+def show_meta_result(context):
+    print("Showing meta") #mock
+
+
+@then("looks for author info using Wikipedia API")
+def wiki_look(context):
+    print("looks for author info using Wikipedia API") #mock
+
+
+@then("program shows info")
+def show_result(context):
+    print("Author Info") #mock
